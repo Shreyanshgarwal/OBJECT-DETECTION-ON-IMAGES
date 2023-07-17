73832a286e2b3667ab6db0854ad27a9942a7bcdc
Shreyansh Agarwal <96068205+Shreyanshgarwal@users.noreply.github.com>
2023-07-17 10:40:44 +0530

    Add files via upload

---
+++ Ac.js
@@ -0,0 +1,34 @@
+img=""
+status="";
+
+function preload() {
+    img = loadImage('AC.jpg')
+}
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status :  Detecting Objects";
+
+}
+function draw() {
+    image(img,0,0,640,420);
+    fill("#00008B")
+    text("AC",45,75);
+    noFill();
+    stroke("#00008B");
+    rect(30,60,450,350);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+ function gotResults(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+ }
+
+
